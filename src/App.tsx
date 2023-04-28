48fe36c35304dfdbc0d126b93f8777ebba6df15b
Achimenes-freeman <gangsta10ip@mail.ru>
2023-04-28 16:36:02 +0300

    feat: add close btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
+
+const tg = window.Telegram.WebApp
+
 export function App() {
+    useEffect(() => {
+        tg.ready()
+    }, [])
+
+    const onClose = () => {
+        tg.close()
+    }
+
     return (
         <div className="App">
             <p>hello telegram</p>
+            <button onClick={onClose}>close</button>
         </div>
     )
 }
