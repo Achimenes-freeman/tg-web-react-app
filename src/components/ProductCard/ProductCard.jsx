26fc46f2b3b4ebd30678c22d83fb7a93c04da462
Achimenes-freeman <gangsta10ip@mail.ru>
2023-05-02 18:26:26 +0300

    feat: change products design

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,15 +1,20 @@
 import styles from './styles.module.scss'
 
-export const ProductCard = ({ title, price, description, thumbnail }) => {
+export const ProductCard = ({ title, price, thumbnail }) => {
     return (
         <div className={styles.ProductCard}>
             <div className={styles.picture}>
                 <img className={styles.image} src={thumbnail} alt={title} />
+                <div className={styles.mask}></div>
             </div>
             <div className={styles.content}>
                 <h3 className={styles.title}>{title}</h3>
-                <p className={styles.description}>{description}</p>
-                <p className={styles.price}>price: {price}</p>
+                <div className={styles.priceBox}>
+                    <p className={styles.price}>
+                        {price}
+                        <span className={styles.currency}> руб.</span>
+                    </p>
+                </div>
             </div>
         </div>
     )
